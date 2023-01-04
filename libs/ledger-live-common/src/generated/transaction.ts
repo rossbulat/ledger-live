602d4205ee5fa80b91b976aea8736d3224071872
Prateek Rathod <prateek@zondax.ch>
2023-01-04 20:26:15 +0530

    refac(icp): rename dfinity to internet_computer

--- libs/ledger-live-common/src/generated/transaction.ts
+++ libs/ledger-live-common/src/generated/transaction.ts
@@ -3,12 +3,12 @@ import cardano from "../families/cardano/transaction";
 import celo from "../families/celo/transaction";
 import cosmos from "../families/cosmos/transaction";
 import crypto_org from "../families/crypto_org/transaction";
-import dfinity from "../families/dfinity/transaction";
 import elrond from "../families/elrond/transaction";
 import ethereum from "../families/ethereum/transaction";
 import evm from "../families/evm/transaction";
 import filecoin from "../families/filecoin/transaction";
 import hedera from "../families/hedera/transaction";
+import internet_computer from "../families/internet_computer/transaction";
 import near from "../families/near/transaction";
 import ripple from "../families/ripple/transaction";
 import solana from "../families/solana/transaction";
@@ -24,12 +24,12 @@ export default {
   celo,
   cosmos,
   crypto_org,
-  dfinity,
   elrond,
   ethereum,
   evm,
   filecoin,
   hedera,
+  internet_computer,
   near,
   ripple,
   solana,
