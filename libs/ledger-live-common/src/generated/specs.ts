f7925a3c076ced4a8ad47bf9d28a3f8fdf73b1f0
Prateek Rathod <prateek@zondax.ch>
2023-01-26 20:28:50 +0530

    feat(icp): add bot tests and integration tests

--- libs/ledger-live-common/src/generated/specs.ts
+++ libs/ledger-live-common/src/generated/specs.ts
@@ -8,6 +8,7 @@ import ethereum from "../families/ethereum/specs";
 import evm from "../families/evm/specs";
 import filecoin from "../families/filecoin/specs";
 import hedera from "../families/hedera/specs";
+import internet_computer from "../families/internet_computer/specs";
 import near from "../families/near/specs";
 import ripple from "../families/ripple/specs";
 import solana from "../families/solana/specs";
@@ -28,6 +29,7 @@ export default {
   evm,
   filecoin,
   hedera,
+  internet_computer,
   near,
   ripple,
   solana,
